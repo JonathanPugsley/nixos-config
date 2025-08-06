c09c4524946cabbfb2f54791e22bcdbc60ff89d3
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2025-08-06 14:25:19 +0100

    update references

---
+++ modules/home/theming/alacritty.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }: {
+  config.programs.alacritty.settings = {
+    colors = with config.colorScheme.palette; {
+      bright = {
+        black = "0x${bg0}";
+        blue = "0x${bright_blue}";
+        cyan = "0x${bright_aqua}";
+        green = "0x${bright_green}";
+        magenta = "0x${bright_purple}";
+        red = "0x${bright_red}";
+        white = "0x${fg0}";
+        yellow = "0x${bright_yellow}";
+      };
+      normal = {
+        black = "0x${bg1}";
+        blue = "0x${bright_blue}";
+        cyan = "0x${bright_aqua}";
+        green = "0x${bright_green}";
+        magenta = "0x${bright_purple}";
+        red = "0x${bright_red}";
+        white = "0x${fg1}";
+        yellow = "0x${bright_yellow}";
+      };
+      cursor = {
+        cursor = "0x${fg0}";
+        text = "0x${fg1}";
+      };
+      primary = {
+        foreground = "0x${fg1}";
+        background = "0x${bg0}";
+      };
+    };
+  };
+}
