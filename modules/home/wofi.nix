5332f629d56683bfd8fc9f3c05d035d4c9e49a5b
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2025-08-06 19:39:01 +0100

    wofi styling

--- modules/home/wofi.nix
+++ modules/home/wofi.nix
@@ -1,14 +1,17 @@
 { config, pkgs, ... }: {
   config.programs.wofi = {
     enable = true;
+    style = builtins.readFile ./theme/wofi-style.css;
     settings = {
       mode = "drun";
+      height = 200;
+      width = 400;
       allow_markup = false;
-      allow_images = true;
-      image_size = 32;
-      term = "alacritty.desktop";
+      allow_images = false;
+      term = "alacritty";
       layer = "top";
       insensitive = true;
+      hide_scroll = true;
       no_actions = true;
     };
   };
