09a2cd21e10f52146e2c0c797ea0fe51c858efe8
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2025-11-03 15:09:55 +0000

    mako tweaks

--- modules/home/desktop/mako/default.nix
+++ modules/home/desktop/mako/default.nix
@@ -4,40 +4,37 @@
   config = lib.mkIf config.modules.mako.enable {
     # libnotify
     home.packages = [ pkgs.libnotify ];
-
     # mako
     services.mako = {
       enable = true;
-      # colours
-      backgroundColor = "#${config.colorScheme.palette.bg0}";
-      borderColor = "#${config.colorScheme.palette.bg1}";
-      textColor = "#${config.colorScheme.palette.fg1}";
-
       settings = {
-        # appearance
+        # window
         layer = "overlay";
         anchor = "top-right";
         width = 400;
         height = 200;
-        margin = "4";
+        # border
         border-size = 2;
         border-radius = 0;
+        border-color = "#${config.colorScheme.palette.fg1}";
+        # style
+        margin = "4";
         padding = "20";
-        max-icon-size = "32";
-        text-alignment = "left";
         format = "<b>%s</b>\\n%b";
+        background-color = "#${config.colorScheme.palette.bg0}";
         "urgency=high".default-timeout = 0;
-        background-opacity = 0;
-
+        # icons
+        max-icon-size = "32";
+        # text
+        text-alignment = "left";
+        text-color = "#${config.colorScheme.palette.fg1}";
         # history
         history = 1;
         max-history = 5;
         max-visible = -1;
-
         # actions
         on-button-left = "invoke-default-action";
         on-button-right = "dismiss";
-
         # misc
         sort = "-time";
         actions = true;
