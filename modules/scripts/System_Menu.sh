d716229363b72d50c14cbe95ec101b451d98f9ab
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2025-11-03 13:06:16 +0000

    rename sysmenu -> System_Menu

--- modules/scripts/System_Menu.sh
+++ modules/scripts/System_Menu.sh
@@ -3,16 +3,16 @@
 
 OPTIONS=("󰒲 Suspend" " Reboot" " Shutdown" "󰍃 Logout" " Lock Screen")
 
-POWER=$( printf "%s\n" "${OPTIONS[@]}" | wofi -dj -L ${#OPTIONS[@]}) || exit 0
+POWER=$( printf "%s\n" "${OPTIONS[@]}" | wofi -dj -L ${#OPTIONS[@]} ) || exit 0
 [[ ! "${OPTIONS[@]}" =~ "$POWER" ]] && exit 1
 
-CONFIRM=$(printf "Yes, $POWER\nCancel" | wofi -dj -L 2)
+CONFIRM=$( printf "Yes, $POWER\nCancel" | wofi -dj -L 2 )
 [[ "$CONFIRM" != "Yes, $POWER" ]] && exit 0
 
 case "${POWER#* }" in
     "Suspend") systemctl suspend;;
     "Reboot") systemctl reboot;;
-    "Shutdown") systemctl shutdown now;;
+    "Shutdown") shutdown now;;
     "Logout") hyprctl dispatch exit;;
     "Lock Screen") hyprlock;;
     *) exit 1
