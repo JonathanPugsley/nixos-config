59aa8b3b27807fb78bc8e88b9c2b32d4f8c78fb4
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2025-12-05 14:55:13 +0000

    fix script names

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -1,29 +1,29 @@
 { pkgs, ... }:
 let
-    lekker-bluetooth-script = pkgs.writeScriptBin "lekker-bluetooth" (builtins.readFile ./lekker-bluetooth.sh);
-    lekker-capture-script = pkgs.writeScriptBin "lekker-capture" (builtins.readFile ./lekker-capture.sh);
-    lekker-resolution-script = pkgs.writeScriptBin "lekker-resolution" (builtins.readFile ./lekker-resolution.sh);
-    lekker-bluelight-script = pkgs.writeScriptBin "lekker-bluelight" (builtins.readFile ./lekker-bluelight.sh);
-    lekker-menu-script = pkgs.writeScriptBin "lekker-menu" (builtins.readFile ./lekker-menu.sh);
-    lekker-notes-script = pkgs.writeScriptBin "lekker-notes" (builtins.readFile ./lekker-notes.sh);
-    lekker-pkg-search-script = pkgs.writeScriptBin "lekker-pkg-search" (builtins.readFile ./lekker-pkg-search.sh);
-    lekker-scripts-script = pkgs.writeScriptBin "lekker-scripts" (builtins.readFile ./lekker-scripts.sh);
-    lekker-system-menu-script = pkgs.writeScriptBin "lekker-system-menu" (builtins.readFile ./lekker-system-menu.sh);
-    lekker-timer-script = pkgs.writeScriptBin "lekker-timer" (builtins.readFile ./lekker-timer.sh);
-    lekker-zenmode-script = pkgs.writeScriptBin "lekker-zenmode" (builtins.readFile ./lekker-zenmode.sh);
+    lekker-bluetooth = pkgs.writeScriptBin "lekker-bluetooth" (builtins.readFile ./lekker-bluetooth.sh);
+    lekker-capture = pkgs.writeScriptBin "lekker-capture" (builtins.readFile ./lekker-capture.sh);
+    lekker-resolution = pkgs.writeScriptBin "lekker-resolution" (builtins.readFile ./lekker-resolution.sh);
+    lekker-bluelight = pkgs.writeScriptBin "lekker-bluelight" (builtins.readFile ./lekker-bluelight.sh);
+    lekker-menu = pkgs.writeScriptBin "lekker-menu" (builtins.readFile ./lekker-menu.sh);
+    lekker-notes = pkgs.writeScriptBin "lekker-notes" (builtins.readFile ./lekker-notes.sh);
+    lekker-pkg-search = pkgs.writeScriptBin "lekker-pkg-search" (builtins.readFile ./lekker-pkg-search.sh);
+    lekker-scripts = pkgs.writeScriptBin "lekker-scripts" (builtins.readFile ./lekker-scripts.sh);
+    lekker-system-menu = pkgs.writeScriptBin "lekker-system-menu" (builtins.readFile ./lekker-system-menu.sh);
+    lekker-timer = pkgs.writeScriptBin "lekker-timer" (builtins.readFile ./lekker-timer.sh);
+    lekker-zenmode = pkgs.writeScriptBin "lekker-zenmode" (builtins.readFile ./lekker-zenmode.sh);
 
     scripts = [
-        lekker-bluetooth-script
-        lekker-capture-script
-        lekker-resolution-script
-        lekker-bluelight-script
-        lekker-menu-script
-        lekker-notes-script
-        lekker-pkg-search-script
-        lekker-scripts-script
-        lekker-system-menu-script
-        lekker-timer-script
-        lekker-zenmode-script
+        lekker-bluetooth
+        lekker-capture
+        lekker-resolution
+        lekker-bluelight
+        lekker-menu
+        lekker-notes
+        lekker-pkg-search
+        lekker-scripts
+        lekker-system-menu
+        lekker-timer
+        lekker-zenmode
     ];
 in {
     config.environment.systemPackages = scripts;
