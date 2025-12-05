5324e46d9ce941f15406f14d5db5d954975e4efa
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2025-12-05 00:17:29 +0000

    + notification modes

--- modules/home/desktop/mako/default.nix
+++ modules/home/desktop/mako/default.nix
@@ -8,40 +8,50 @@
     services.mako = {
       enable = true;
       settings = {
-        # window
-        layer = "overlay";
-        anchor = "top-right";
-        width = 400;
-        height = 200;
-        # border
-        border-size = 2;
-        border-radius = 0;
-        border-color = "#${config.colorScheme.palette.fg1}";
-        # style
-        margin = "4";
-        padding = "20";
-        format = "<b>%s</b>\\n%b";
-        background-color = "#${config.colorScheme.palette.bg0}";
-        "urgency=high".default-timeout = 0;
-        # icons
-        max-icon-size = "32";
-        # text
-        text-alignment = "left";
-        text-color = "#${config.colorScheme.palette.fg1}";
-        # history
-        history = 1;
-        max-history = 5;
-        max-visible = -1;
+        # global
+        max-history = 20;
+        sort = "+time";
         # actions
         on-button-left = "invoke-default-action";
         on-button-right = "dismiss";
-        # misc
-        sort = "+time";
-        actions = true;
-        markup = true;
+        on-button-middle = "none";
+        on-touch = "invoke-default-action";
+        on-notify = "none";
+        # style
+        font = "JetBrainsMono 10";
+        background-color = "#${config.colorScheme.palette.bg0}E6";
+        text-color = "#${config.colorScheme.palette.fg1}FF";
+        width = 300;
+        height = 100;
+        outer-margin=0;
+        margin=4;
+        padding = 14;
+        border-size = 2;
+        border-color = "#${config.colorScheme.palette.bg0}FF";
+        border-radius = 0;
+        progress-color = "none";
         icons = true;
+        max-icon-size = 32;
+        icon-path = "";
+        icon-location = "left";
+        icon-border-radius = 0;
+        markup = true;
+        actions = true;
+        history = 1;
+        format = "<b>%s</b>\\n%b";
+        text-alignment = "left";
         default-timeout = 5000;
         ignore-timeout = false;
+        group-by = "none";
+        max-visible = 5;
+        output = "";
+        layer = "overlay";
+        anchor = "top-right";
+
+        "mode=dnd".invisible = 1;
+        "mode=zen".invisible = 1;
+        "mode=zen app-name=notify-send".invisible = 0;
+        "urgency=high".default-timeout = 0;
       };
     };
   };
--- modules/scripts/lekker-zenmode.sh
+++ modules/scripts/lekker-zenmode.sh
@@ -11,9 +11,11 @@ read_mode() {
 
 toggle_mode() {
     if [[ "$MODE" == "zen" ]]; then
+        makoctl mode -r zen
         echo "normal" > "/tmp/zenmode"
         echo "{\"alt\": \"normal\"}"
     else
+        makoctl mode -s zen
         echo "zen" > "/tmp/zenmode"
         echo "{\"alt\": \"zen\"}"
     fi
