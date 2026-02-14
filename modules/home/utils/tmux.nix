b68828e7d7d7ac7b40e702b85403202f1ff82ed4
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2026-02-14 21:33:29 +0000

    tmux styling

--- modules/home/utils/tmux.nix
+++ modules/home/utils/tmux.nix
@@ -12,15 +12,32 @@
       historyLimit = 1000;
       shell = "${pkgs.zsh}/bin/zsh";
 
-      extraConfig = "
-        set -g renumber-windows on
-        set -g status-position top
+      extraConfig = let
+        colours = {
+          background = "#${config.lib.stylix.colors.base00}";
+          foreground = "#${config.lib.stylix.colors.base05}";
+          green = "#${config.lib.stylix.colors.base0B}";
+        };
+      in "
+        set -g status-position bottom
         set -g status-justify absolute-centre
         set -g status-style \"bg=default\"
+        set -g renumber-windows on
+
         set -g window-status-style \"fg=default\"
-        set -g window-status-current-style \"fg=cyan bold\"
-        set -g status-right \"#S\"
-        set -g status-left \"\"
+        set -g window-status-format \" #I:#W \"
+        set -g window-status-current-style \" bg=${colours.green} fg=${colours.background} bold\"
+        set -g window-status-current-format \" #I:#W \"
+        set -g window-status-separator \"\"
+
+        set -g status-right \" #h \"
+        set -g status-right-style \"bg=${colours.green} fg=${colours.background} bold\"
+        set -g status-left \" #S \"
+        set -g status-left-style \"bg=${colours.green} fg=${colours.background} bold\"
+
+        set -g message-command-style \"bg=${colours.background}\"
+        set -g message-style \"bg=${colours.background}\"
+        set -g prompt-cursor-colour \"${colours.foreground}\"
 
         set -g escape-time 5
 
