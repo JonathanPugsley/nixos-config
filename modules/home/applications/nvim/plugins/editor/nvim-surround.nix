5ea89fe2e7a9faf5fe828a1abec6a9d2b09bcdad
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2026-06-08 14:49:05 +0100

    remove deprecated config

--- modules/home/applications/nvim/plugins/editor/nvim-surround.nix
+++ modules/home/applications/nvim/plugins/editor/nvim-surround.nix
@@ -3,20 +3,6 @@
     programs.nixvim = {
       plugins.nvim-surround = {
         enable = true;
-
-        settings.keymaps = {
-          insert = "<C-g>s"; # not sure
-          insert_line = "<C-g>S"; # not sure
-          normal = "ys"; # enter surround mode, end with char
-          normal_cur = "yss"; # quick surround line
-          normal_line = "yS"; # not sure
-          normal_cur_line = "ySS"; # brackets on new lines
-          visual = "S"; # enable when in visual mode
-          visual_line = "gS";
-          delete = "ds";
-          change = "cs";
-          change_line = "cS";
-        };
       };
     };
   };
