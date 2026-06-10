9b161cd217dfc1810627d86bb2f75ae1dabba5f4
Jonny <129212089+JonathanPugsley@users.noreply.github.com>
2026-06-10 18:52:33 +0100

    gum styling

--- modules/home/utils/gum.nix
+++ modules/home/utils/gum.nix
@@ -2,6 +2,17 @@
   options.modules.gum.enable = lib.mkEnableOption "enable gum";
 
   config = lib.mkIf config.modules.gum.enable {
-    home.packages = [ pkgs.gum ];
+    home = {
+      packages = [ pkgs.gum ];
+      sessionVariables = {
+        # choose
+        GUM_CHOOSE_CURSOR = "❯ ";
+        GUM_CHOOSE_CURSOR_FOREGROUND = "#${config.lib.stylix.colors.base0D}";
+        GUM_CHOOSE_HEADER_FOREGROUND = "#${config.lib.stylix.colors.base0B}";
+        GUM_CHOOSE_ITEM_FOREGROUND = "#${config.lib.stylix.colors.base05}";
+        GUM_CHOOSE_SELECTED_FOREGROUND = "#${config.lib.stylix.colors.base0D}";
+        GUM_CHOOSE_SHOW_HELP = false;
+      };
+    };
   };
 }
